90054f1a8feb6a77a446a9a7f9f4f00cbfd8c45f
mishina naoya <msnyxx811@gmail.com>
2023-11-11 16:16:47 +0900

    共通レイアウトの設定

---
+++ src/components/Layout.tsx
@@ -0,0 +1,13 @@
+type LayoutProps = {
+  children: React.ReactNode
+}
+
+const Layout = ({children}: LayoutProps) => {
+  return (
+    <main>
+      {children}
+    </main>
+  )
+}
+
+export default Layout
